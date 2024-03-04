89c621789d25a0071efdf7bca95f8e33df1c518d
babygrandad <relebogilenkotswe@gmail.com>
2024-03-04 09:20:35 +0000

    sort of hello world with betty sandards

---
+++ hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*
+ * main, Writting my first C programme, but I love Javascript, sad.
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
