2881173cf88f523921423926951b2919be026ad6
root <babygrandad>
2024-03-07 14:07:00 +0000

    print putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+/**
+  * main - Print putchar
+  *
+  * Return: Always 0
+  */
+int main(void)
+{
+    char c[] = "_putchar\n";
+    int i;
+
+    for (i = 0; c[i] != '\0'; i++)
+    {
+        _putchar(c[i]);
+    }
+
+    return (0);
+}
