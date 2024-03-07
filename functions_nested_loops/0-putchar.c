e7354cd51a6a9ad9e494914a383c7a20ec690438
root <babygrandad>
2024-03-07 14:16:43 +0000

    putting lines after decleration

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,16 +1,18 @@
 /**
-  * main - Print putchar
-  *
-  * Return: Always 0
-  */
+ * main - Print putchar
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-	int _putchar(char c);
-	char c[] = "_putchar\n";
-	int i;
-	for (i = 0; c[i] != '\0'; i++)
-	{
-	_putchar(c[i]);
-	}
-	return (0);
+    int _putchar(char c);
+    char c[] = "_putchar\n";
+    int i;
+
+    for (i = 0; c[i] != '\0'; i++)
+    {
+        _putchar(c[i]);
+    }
+
+    return (0);
 }
