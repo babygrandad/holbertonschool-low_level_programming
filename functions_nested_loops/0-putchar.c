cb85a671acca0a154eb57c601a711071bbfd94f5
root <babygrandad>
2024-03-07 14:17:52 +0000

    fixing indentation

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -5,14 +5,14 @@
  */
 int main(void)
 {
-    int _putchar(char c);
-    char c[] = "_putchar\n";
-    int i;
+int _putchar(char c);
+char c[] = "_putchar\n";
+int i;
 
-    for (i = 0; c[i] != '\0'; i++)
-    {
-        _putchar(c[i]);
-    }
+for (i = 0; c[i] != '\0'; i++)
+{
+_putchar(c[i]);
+}
 
-    return (0);
+return (0);
 }
