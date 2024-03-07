e569f905152939b317a7d8611eb5d47eba1f09bf
babygrandad <relebogilenkotswe@gmail.com>
2024-03-07 11:35:12 +0000

    trying somthing different to see if it works

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <time.h>
+/* main - function to be ran.
+ * Return: (0) succsess.
+ *
+ */
+
+int main (void)
+{	
+
+	int n;
+	
+	if(n > 0)
+	{
+	printf(n + " is positive");\n
+	}
+	else if ( n < 0)
+	{
+	printf(n + " is negative\n");
+	}
+	else	
+	{ 
+	printf(n +" is zero\n";
+	}
+}
