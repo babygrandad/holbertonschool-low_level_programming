920c2f603dbfb8d07b1b372f4bcd4de21be6f6e0
babygrandad <relebogilenkotswe@gmail.com>
2024-03-07 12:39:25 +0000

    conditional statements

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
-
 
 /**
 * main - running conditional statements.
@@ -11,9 +9,7 @@
 int main(void)
 {
 	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n < 0)
