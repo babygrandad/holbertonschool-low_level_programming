9fb93c61109e4eec0542fff0df3a40fdfcffbe2c
babygrandad <relebogilenkotswe@gmail.com>
2024-03-08 18:20:56 +0000

    conditional printing

--- functions_nested_loops/5-main.c
+++ functions_nested_loops/5-main.c
@@ -8,17 +8,22 @@
 
 int print_sign(int n)
 {
-	if(n > 0) {
+	if (n > 0)
+	{
 	_putchar('+');
 	_putchar('\n');
-	return 1;
-	} else if (n < 0) {
+	return (1);
+	}
+	else if (n < 0)
+	{
 	_putchar('-');
 	_putchar('\n');
-	return -1;
-	} else {
+	return (-1);
+	}
+	else
+	{
 	_putchar('0');
 	_putchar('\n');
-	return 0;
+	return (0);
 	}
 }
