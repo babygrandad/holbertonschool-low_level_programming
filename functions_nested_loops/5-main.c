03b2bf6855ea456009cc56ca31120efb9202acc7
babygrandad <relebogilenkotswe@gmail.com>
2024-03-08 17:59:07 +0000

    conditional print

---
+++ functions_nested_loops/5-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int print_sign(int n)
+{
+	if(n > 0) {
+	_putchar('+');
+	_putchar('\n');
+	return 1;
+	} else if (n < 0) {
+	_putchar('-');
+	_putchar('\n');
+	return -1;
+	} else {
+	_putchar('0');
+	_putchar('\n');
+	return 0;
+	}
+}
