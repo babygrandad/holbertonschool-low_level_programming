180e282a39cef8d811ca56290b8d6e7d040174fb
babygrandad <relebogilenkotswe@gmail.com>
2024-03-08 14:01:40 +0000

    alphabet is making me cry

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,19 +1,12 @@
 #include "main.h"
 
-/**
-  * print_alphabet - prints out the alhpabet in lowercase
-  * Return: return 0 always
-  */
-
 void print_alphabet(void)
 {
 
-	char i;
-
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		_putchar(i);
-	}
-
-	_putchar('\n');
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
 }
