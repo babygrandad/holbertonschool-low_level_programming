d619642f84cd7c818e3a33cbdaf6b025779e5e9b
babygrandad <relebogilenkotswe@gmail.com>
2024-03-11 06:21:56 +0000

    is upper - probably gonna faiil as they all do at first

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - this function checks if the charachter is upercase 
+ * @c: this is the variable being checked.
+ * Return : Well, this depends on the charachter if the return is 0 or 1.
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && C <=90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
