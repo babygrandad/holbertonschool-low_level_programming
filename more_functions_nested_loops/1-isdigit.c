3cfb0a79bd3a3b86af0b53bc33e0bdb707f3e6c2
babygrandad <relebogilenkotswe@gmail.com>
2024-03-11 06:44:20 +0000

    is digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - this checks if a charachter is a didgit or not.
+ * @c: is the value being checked.
+ * Return: will either return 1 or 0 zero depending on the condition
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && if c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
