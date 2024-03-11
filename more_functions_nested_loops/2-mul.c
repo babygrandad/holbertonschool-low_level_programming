1bf812ee7e8c01108c2a843599e67be7ee75a91f
babygrandad <relebogilenkotswe@gmail.com>
2024-03-11 07:57:26 +0000

    mutiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - check the code
+ * @a: the first interger
+ * @b: the second interger
+ * Return: the answer of both intergers multiplied
+ */
+
+int mul(int a, int b)
+{
+	retun ( a * b );
+}
