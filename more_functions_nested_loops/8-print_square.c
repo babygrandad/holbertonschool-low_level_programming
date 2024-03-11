5df7ddbcca3cad7ef3026eda92d4573b167796ea
babygrandad <relebogilenkotswe@gmail.com>
2024-03-11 13:00:52 +0000

    squares

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * print_square - prints sqaures.
+ * Return: returns the numbers.
+ * @n: the number to be sqaured.
+ */
+void print_square(int n)
+{
+	int i, j;
+
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= n; j++)
+		{
+			_putchar('#');
+
+			if (j == n)
+			{
+				_putchar('\n');
+			}
+		}
+		_putchar('#')
+	}
+}
