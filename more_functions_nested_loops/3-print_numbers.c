7d24359c9eb2262feb091674198906d14acc3453
babygrandad <relebogilenkotswe@gmail.com>
2024-03-11 08:34:56 +0000

    numbers speak for themmselves

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * void print_numbers - prints numbers 0 to 9.
+ * Return: returns the numbers.
+ */
+void print_numbers(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
