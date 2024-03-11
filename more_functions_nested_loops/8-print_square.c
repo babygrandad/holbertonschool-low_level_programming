d616ffdd9e2fbba3c05c0c93cf39c82b4014bd4e
babygrandad <relebogilenkotswe@gmail.com>
2024-03-11 13:38:58 +0000

    squares

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,19 +1,28 @@
 #include "main.h"
+
 /**
- * print_square - prints sqaures.
- * Return: returns the numbers.
- * @n: the number to be sqaured.
+ * print_square - prints a square, followed by a new line;
+ * @n: nunber to be squared
  */
 void print_square(int n)
 {
-	int i, j;
+	if (n <= 0)
+	{
+	    _putchar('\n');
+	}
+	else
+	{
+	    int i, j;
+    }
 
 	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j < n; j++)
-		{
-			_putchar('#');
-		}
-		_putchar('\n');
+	    for (j = 0; j < n; j++)
+	    {
+	        _putchar('#');
+	    }
+
+	    _putchar('\n');
 	}
 }
+
