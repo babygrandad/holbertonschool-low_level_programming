aa993aeaf9d7ccccb34e463f010c2cfd0166a7d2
babygrandad <relebogilenkotswe@gmail.com>
2024-03-11 06:29:19 +0000

    fixing betty standardsw

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 /**
- * _isupper - this function checks if the charachter is upercase 
+ * _isupper - this function checks if the charachter is upercase
  * @c: this is the variable being checked.
- * Return : Well, this depends on the charachter if the return is 0 or 1.
+ * Return: Well, this depends on the charachter if the return is 0 or 1.
  */
 
 int _isupper(int c)
 {
-	if (c >= 65 && C <=90)
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
