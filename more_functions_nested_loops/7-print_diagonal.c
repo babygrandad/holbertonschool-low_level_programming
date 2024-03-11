d136c5116e0a492d88f4a9149c877c060721b9b1
babygrandad <relebogilenkotswe@gmail.com>
2024-03-11 10:53:15 +0000

    diagonals

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * print_diagonal - prints a diagonal i guess.
+ * @n: is the number of times the \ is typed i guess.
+ * Return: returns the line.
+ */
+void print_diagonal(int n)
+{
+
+	int i;
+
+	for (i = 1; i <= n; i++)
+	{
+		_putchar('\\');
+	}
+	_putchar('\n');
+}
