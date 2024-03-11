ac5dd100f6073047a0ce317c01e60a1ce3e57322
babygrandad <relebogilenkotswe@gmail.com>
2024-03-11 14:15:28 +0000

    finish at home

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints fizz buzz game;
+ * Return: Retuens fizz buzz 1 - 100
+ */
+
+int main ()
+{
+    int i;
+    for(i = 1; <= 100; i++)
+    {
+        if ((i % 3 == 0 ) && (i % 5 == 0 ))
+        {
+            printf('FizzBuzz ');
+        }
+        else if (i % 3 == 0 )
+        {
+            printf('Fizz ');
+        }
+        else if (i % 5 == 0 )
+        {
+            printf('Buzz ');
+        }
+        else
+        {
+            printf(i + " ");
+        }
+    }
+}
+
