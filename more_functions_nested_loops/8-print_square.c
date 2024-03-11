4a531d4037da4ce6ce51f24b4207bd54941ff68c
babygrandad <relebogilenkotswe@gmail.com>
2024-03-11 13:42:59 +0000

    squars

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -8,21 +8,19 @@ void print_square(int n)
 {
 	if (n <= 0)
 	{
-	    _putchar('\n');
+		_putchar('\n');
 	}
 	else
 	{
-	    int i, j;
-    }
+		int i, j;
+	}
 
 	for (i = 0; i < n; i++)
 	{
-	    for (j = 0; j < n; j++)
-	    {
-	        _putchar('#');
-	    }
-
-	    _putchar('\n');
+		for (j = 0; j < n; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
 	}
 }
-
