29af13a245b0af68fd4cdb36279a4e7988208ae5
babygrandad <relebogilenkotswe@gmail.com>
2024-03-12 12:10:00 +0000

    backwards tree

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -17,12 +17,12 @@ void print_triangle(int size)
 
 	for (i = 1; i <= size; i++)
 	{
-		for (j = size - 1; j > 0; j--)
+		for (j =  1; j <= size - 1; j++)
 		{
 			_putchar(' ');
 		}
 
-		for (j = 0; j < i; j++)
+		for (j = 1; j <= i; j++)
 		{
 			_putchar('#');
 		}
